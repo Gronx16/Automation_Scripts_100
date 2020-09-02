f4d29c2ec1e8e4783c619410ed91063bf3df8cb0
gronx16 <gronx16@github.com>
2020-09-02 13:58:02 +0530

    Third Scrit added

--- whois_check.py
+++ whois_check.py
@@ -5,8 +5,12 @@ website=input("Enter domain name (eg. google.com): ")
 print()
 try:
     whois_output=list((check_output("whois {}".format(website), shell=True).decode("UTF-8")).splitlines())
+    cat=0
     for ch in whois_output:
         if "Domain Name" in ch:
+            cat+=1
+            if cat>1:
+                break 
             print(ch)
         elif "Registry Domain ID" in ch:
             print(ch)
@@ -19,11 +23,13 @@ try:
         elif "Registry Expiry Date" in ch:
             print(ch)
         elif "Name Server" in ch:
-            print(ch)
+            print(ch)   
 except Exception as ex:
     print("Enter the correct domain name")
     print()
 finally:
+    print("\n\n")
     print("Thanks for using the script")
+
 
 print("-------------------------------------------------------------------------------------------------")
